66ab54089d8e22d413ae261b70697afd821a211a
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-05-15 23:49:27 +0530

    main

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:flutter/services.dart';
+import 'package:snap_split/provider/puzzle_provider.dart';
+
+
+import 'package:snap_split/screens/splash_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => PuzzleProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SnapSplit',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF6750A4),
+          brightness: Brightness.light,
+        ),
+        fontFamily: 'Poppins',
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF6750A4),
+          brightness: Brightness.dark,
+        ),
+        fontFamily: 'Poppins',
+      ),
+      themeMode: ThemeMode.system,
+      home: const SplashScreen(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
